f605727f4df1b2af86923dd01050371c06f75bd9
Nadeem <m.nadeem83005@gmail.com>
2022-12-19 20:00:25 +0530

    reformat file and corrected gramamr

--- Scripts/fibinocci-even-sum.py
+++ Scripts/fibinocci-even-sum.py
@@ -1,37 +1,37 @@
 while True:
     try:
-        testcase = int(input("enter no of test cases"))
+        testcase = int(input("Enter no of test cases: "))
         break
     except:
-        print("enter valid values")
+        print("Enter valid values")
         continue
-if testcase<=0:
-    print("invalid")
+if testcase <= 0:
+    print("Invalid")
 else:
-    l = [1,2]
-    j=1
-    p =1
-    while p<=testcase:
+    l = [1, 2]
+    j = 1
+    p = 1
+    while p <= testcase:
         try:
-            n = int(input("Enter till range"))
+            n = int(input("Enter till range: "))
         except:
-            print("write valid values")
+            print("Enter valid values")
             continue
-        j=1
-        l=[1,2]
-        if n>2:
-            while l[j]<=n:
-                sum = l[j-1] + l[j]
+        j = 1
+        l = [1, 2]
+        if n > 2:
+            while l[j] <= n:
+                sum = l[j - 1] + l[j]
                 l.append(sum)
-                j+=1
-            l.remove(l[len(l)-1])
+                j += 1
+            l.remove(l[len(l) - 1])
             add = 0
             for i in l:
-                if i%2==0:
-                    add+=i
+                if i % 2 == 0:
+                    add += i
                 else:
                     continue
-            print(add)
-            p+=1
+            print("Sum of Even Numbers In Fibinocci Series", add)
+            p += 1
         else:
             print("please enter valid values")
